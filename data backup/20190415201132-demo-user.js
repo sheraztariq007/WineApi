a21e105b56da0bec79b653f3fb5f59a8c1400f60
abdullahmasood <netroxtech1122@gmail.com>
2019-05-16 10:57:51 -0700

    change in db

--- data backup/20190415201132-demo-user.js
+++ data backup/20190415201132-demo-user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-        up: (queryInterface, Sequelize) => {
+    up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('usuarios', [
             {
                 email: 'test@gmail.com',
@@ -27,17 +27,34 @@ module.exports = {
                 role_id:'3',
                 name:'Tom',
                 surname:'jery'
+            },
+            {
+                email: 'albertoortega@valtravieso.com',
+                password:'e10adc3949ba59abbe56e057f20f883e',
+                company:'1',
+                role_id:'2',
+                name:'Alberto',
+                surname:'Ortega'
+            },
+            {
+                email: 'ricardovelasco@valtravieso.com',
+                password:'e10adc3949ba59abbe56e057f20f883e',
+                company:'1',
+                role_id:'2',
+                name:'Ricardo',
+                surname:'Velasco'
             }
+
         ], {});
-},
+    },
 
-down: (queryInterface, Sequelize) => {
-    /*
-     Add reverting commands here.
-     Return a promise to correctly handle asynchronicity.
+    down: (queryInterface, Sequelize) => {
+        /*
+         Add reverting commands here.
+         Return a promise to correctly handle asynchronicity.
 
-     Example:
-     return queryInterface.bulkDelete('People', null, {});
-     */
-}
+         Example:
+         return queryInterface.bulkDelete('People', null, {});
+         */
+    }
 };
--- seeders/20190415201132-demo-user.js
+++ seeders/20190415201132-demo-user.js
@@ -3,31 +3,6 @@
 module.exports = {
         up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('usuarios', [
-            {
-                email: 'test@gmail.com',
-                password:'e10adc3949ba59abbe56e057f20f883e',
-                company:'1',
-                role_id:'2',
-                name:'abdullah',
-                surname:'masood'
-            },
-            {
-                email: 'manager@gmail.com',
-                password:'e10adc3949ba59abbe56e057f20f883e',
-                company:'1',
-                role_id:'2',
-                name:'Tom',
-                surname:'jery'
-            }
-            ,
-            {
-                email: 'user@gmail.com',
-                password:'e10adc3949ba59abbe56e057f20f883e',
-                company:'1',
-                role_id:'3',
-                name:'Tom',
-                surname:'jery'
-            },
              {
                 email: 'albertoortega@valtravieso.com',
                 password:'e10adc3949ba59abbe56e057f20f883e',
