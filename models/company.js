42508d25afe34e7ec8cf166bd2ce5a6898785ba8
abdullahmasood <netroxtech1122@gmail.com>
2019-06-10 05:08:53 -0700

    add module options

--- models/company.js
+++ models/company.js
@@ -3,6 +3,11 @@ module.exports = (sequelize, DataTypes) => {
   const Company = sequelize.define('companies', {
     name: DataTypes.STRING,
     contactMail: DataTypes.STRING,
+    module_tasks: DataTypes.BOOLEAN,
+    module_disease: DataTypes.BOOLEAN,
+    module_mantain: DataTypes.BOOLEAN,
+    module_sampling: DataTypes.BOOLEAN,
+    module_notefield: DataTypes.BOOLEAN,
     contactPhone: DataTypes.INTEGER,
     createDate: DataTypes.DATE
   }, {});
