cc165c59e94adac66a076dd12ad435423097fcec
abdullahmasood <netroxtech1122@gmail.com>
2019-06-10 06:48:04 -0700

    f

--- database/db_sql.js
+++ database/db_sql.js
@@ -38,7 +38,7 @@ module.exports = {
             " comp.module_sampling as comp_sampling," +
             " comp.module_disease as comp_disease, " +
             "comp.module_notefield as comp_notefield," +
-            "comp.module_tasks as comp_tasks  "  +
+            "comp.module_tasks as comp_tasks, comp.name as company_name  "  +
             "from usuarios,user_roles,companies as comp where" +
             " usuarios.email='"+email+"' AND usuarios.password='"+md5(password)+"' AND usuarios.role_id=user_roles.id " +
             "AND usuarios.company=comp.id LIMIT 1",(err,resp)=>{
