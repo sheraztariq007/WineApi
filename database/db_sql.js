678d0c951db094aada96100b0d3038795672ec64
abdullahmasood <netroxtech1122@gmail.com>
2019-06-17 10:22:55 -0700

    f

--- database/db_sql.js
+++ database/db_sql.js
@@ -918,6 +918,101 @@ module.exports = {
                 }
             });
         }else if (req.body.form_type == 2) {
+            client.query("select usuarios.name as username,m_f.marchinar_id,m_f.start_date,m_f.trabajador as trabajador" +
+                ",maquinaria.name as maquinaria_name," +
+                "m_f.tratamiento as tratamiento,m_f.dosis,m_f.observaciones as observaciones," +
+                "m_f.product,m_f.location,m_f.reported_date_time,fb.name as field_name " +
+                " from  module_fieldnotebooks as m_f,usuarios,fields as fb,maquinaria where " +
+                " m_f.reportedby_user_id=usuarios.id AND m_f.field_id=fb.id AND " +
+                "form_type='"+req.body.form_type+"' AND maquinaria.id=m_f.marchinar_id " +
+                "AND m_f.company_id='"+req.body.company_id+"'", (err, result)=> {
+                console.log(err,result);
+                if(result.rowCount>0) {
+                    res.send({
+                        "status": 200,
+                        "data": result.rows
+                    });
+                }else{
+                    res.send({
+                        "status": 204,
+                        "messgae": "Sorry Not Record Found"
+                    });
+                }
+            });
+
+        }
+        else if (req.body.form_type == 3) {
+            client.query("select usuarios.name as username,m_f.trabajador as trabajador,m_f.start_date," +
+                "m_f.tipodeabonado as tipodeabonado,m_f.dosis,m_f.observaciones as observaciones," +
+                "m_f.product,m_f.location,m_f.reported_date_time,fb.name as field_name " +
+                " from  module_fieldnotebooks as m_f,usuarios,fields as fb where " +
+                " m_f.reportedby_user_id=usuarios.id AND m_f.field_id=fb.id AND " +
+                "form_type='"+req.body.form_type+"' " +
+                "AND m_f.company_id='"+req.body.company_id+"'", (err, result)=> {
+                console.log(err,result);
+                if(result.rowCount>0) {
+                    res.send({
+                        "status": 200,
+                        "data": result.rows
+                    });
+                }else{
+                    res.send({
+                        "status": 204,
+                        "messgae": "Sorry Not Record Found"
+                    });
+                }
+            });
+
+        }
+        else if (req.body.form_type == 4) {
+            client.query("select usuarios.name as username,m_f.start_date,m_f.subparcela as subparcela," +
+                "m_f.horasderiego as horasderiego," +
+                "m_f.dosis,m_f.observaciones as observaciones," +
+                "m_f.location,m_f.reported_date_time,fb.name as field_name" +
+                " from  module_fieldnotebooks as m_f,usuarios,fields as fb  where " +
+                " m_f.reportedby_user_id=usuarios.id AND m_f.field_id=fb.id AND " +
+                "form_type='"+req.body.form_type+"' " +
+                "AND m_f.company_id='"+req.body.company_id+"'", (err, result)=> {
+                console.log(err,result);
+                if(result.rowCount>0) {
+                    res.send({
+                        "status": 200,
+                        "data": result.rows
+                    });
+                }else{
+                    res.send({
+                        "status": 204,
+                        "messgae": "Sorry Not Record Found"
+                    });
+                }
+            });
+
+        }
+    },getNoteFieldBooksWeb:function(req,res) {
+        if (req.body.form_type == 1) {
+            client.query("select usuarios.name as username,m_f.marchinar_id,m_f.start_date, m_f.end_date,maquinaria.name as maquinaria_name," +
+                "m_f.product,m_f.app_method,m_f.surface,m_f.location,m_f.reported_date_time," +
+                "fb.name as field_name,lb.name as labor_name from  module_fieldnotebooks as m_f, labors as lb,fields as fb," +
+                " usuarios,maquinaria where " +
+                " m_f.reportedby_user_id=usuarios.id AND " +
+                "m_f.labore_id=lb.id AND m_f.field_id=fb.id AND" +
+                " m_f.form_type='"+req.body.form_type+"'" +
+                " AND m_f.company_id='"+req.body.company_id+"'  AND " +
+                "maquinaria.id=m_f.marchinar_id  order by reported_date_time", (err, result)=> {
+                console.log(result);
+                if(result.rowCount>0) {
+                    res.send({
+                        "status": 200,
+                        "data": result.rows
+                    });
+                }else{
+                    res.send({
+                        "status": 204,
+                        "messgae": "Sorry Not Record Found"
+                    });
+                }
+            });
+        }else if (req.body.form_type == 2) {
             client.query("select usuarios.name as username,m_f.marchinar_id,m_f.start_date,m_f.trabajador as trabajador," +
                 "m_f.tratamiento as tratamiento,m_f.dosis,m_f.observaciones as observaciones," +
                 "m_f.product,m_f.location,m_f.reported_date_time,fb.name as field_name " +
