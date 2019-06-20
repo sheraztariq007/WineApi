6b9a2696d14765c2637d2712c169154bb66c3ce5
abdullahmasood <netroxtech1122@gmail.com>
2019-06-20 00:48:21 -0700

    working

--- database/db_sql.js
+++ database/db_sql.js
@@ -558,19 +558,17 @@ module.exports = {
         });
     },
     getmaquinarialist(req,res){
-       client.query("select *from maquinaria where company_id='"+req.body.company_id+"'",(err,resp)=>{
+       client.query("select *from maquinaria where company_id='"+req.body.company_id+"' AND id >1",(err,resp)=>{
            console.log(err,resp);
-           if(resp.rowCount>0){
-               res.send({
-                   "status":200,
-                   "data":resp.rows
-               })
-           }else{
-               res.send({
-                   "status":204,
-                   "message":"Empty Notifications"
-               })
-           }
+
+              client.query("select *from maquinaria where id=1",(err,resp1)=>{
+                  res.send({
+                      "status":200,
+                      "data":resp.rows,
+                      "none":resp1.rows
+                  })
+              });
+
         });
     },
     readMyNotifications:function(req,res){
