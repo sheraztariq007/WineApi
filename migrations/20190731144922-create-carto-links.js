2e5aefd687d1ec96c2028f669a4e7d50e9899a6f
abdullahmasood <netroxtech1122@gmail.com>
2019-07-31 12:50:10 -0700

    a

---
+++ migrations/20190731144922-create-carto-links.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('carto_links', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      company_id: {
+        type: Sequelize.INTEGER
+      },
+      show_name: {
+        type: Sequelize.TEXT
+      },
+      link: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now")
+
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('carto_links');
+  }
+};
